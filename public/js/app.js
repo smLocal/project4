b6cdd0037caded3bb23c9878d327f1b0d2955d43
david szyszkowski <try2murkme@gmail.com>
2016-05-24 16:46:50 -0700

    made email partial work too

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,11 @@ function MainRouter($stateProvider, $urlRouterProvider) {
       // method: 'GET',
       url: "/login",
       templateUrl: "partials/login.html",
+    })
+    .state('email', {
+      // method: 'GET',
+      url: "/email",
+      templateUrl: "partials/email.html",
     });
 
 }
--- public/js/users_controller.js
+++ public/js/users_controller.js
@@ -1,9 +1,13 @@
 angular.module("myApp")
   .controller("UsersController", UsersController);
 
-
+//UsersController.inject$http
 
 function UsersController() {
   var self = this
   self.message = "hello there"
+
 }
+
+
+//function login($http)
