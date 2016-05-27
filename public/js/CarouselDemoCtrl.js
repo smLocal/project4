fd88deff2b40511c59eaefa21d8af181b04d85cc
david szyszkowski <try2murkme@gmail.com>
2016-05-26 17:19:30 -0700

    tried materialize carousel and parallax to no avail

---
+++ public/js/CarouselDemoCtrl.js
@@ -0,0 +1,13 @@
+// angular.module('myApp')
+//   .controller('CarouselDemoCtrl', CarouselDemoCtrl);
+
+
+// function CarouselDemoCtrl() {
+//   var self = this;
+//   this.myInterval = 5000;
+//   this.noWrapSlides = false;
+//   this.active = 0;
+//   this.slides = [];
+//   this.currIndex = 0;
+
+// }
---
+++ public/js/materialize.js
@@ -0,0 +1,12 @@
+console.log('matty is loaded');
+
+// $(document).ready(function(){
+//   $('.parallax').parallax();
+// });
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementsByClassName('parallax').parallax();
+});
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   parallax();
+// });
